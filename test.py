7dc2d2ed976adac5e888bada7d682ed82dfd436a
SamHDev <sam02h.huddart@gmail.com>
2018-11-05 02:12:58 +0000

    av

---
+++ test.py
@@ -0,0 +1,11 @@
+from samNeuralNet import SamNeuralNet as snn
+
+if (snn.isNetwork("network.snn")):
+    net = snn.loadNetwork("network.snn")
+    print("Loaded")
+else:
+    net = snn.createNetwork(3,4,4,2)
+    snn.saveNetwork(net,"network.snn")
+    print("Created")
+
+print(net.name)
